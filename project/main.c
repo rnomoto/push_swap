26126dc2e18c1c6e50c5d33c5afa3d232664825e
rnomoto <r.nomoto12345@gmail.com>
2025-05-04 15:18:43 +0900

    quick sort and base sort?

--- project/main.c
+++ project/main.c
@@ -6,7 +6,7 @@
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 15:18:42 by rnomoto           #+#    #+#             */
-/*   Updated: 2025/04/26 19:18:54 by rnomoto          ###   ########.fr       */
+/*   Updated: 2025/05/03 12:33:48 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int main(int argc, char **argv)
 	int *stack_a;
 	int i;
 
+	if (argc == 1)
+		return 0;
 	stack_a = (int *)malloc(sizeof(int) * argc);
 	if (stack_a == NULL)
 		return -1;
