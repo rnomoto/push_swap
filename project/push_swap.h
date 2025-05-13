3b5ad31968f43da2e20946e875ea0c1a614e71b8
rnomoto <r.nomoto12345@gmail.com>
2025-05-13 11:27:55 +0900

    need libft?

--- project/push_swap.h
+++ project/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 15:24:52 by rnomoto           #+#    #+#             */
-/*   Updated: 2025/04/26 20:48:21 by rnomoto          ###   ########.fr       */
+/*   Updated: 2025/05/13 11:09:13 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,11 @@
 
 #include <unistd.h>
 #include <stdlib.h>
+#include <limits.h>
 
+int	ft_atoi(const char *str);
+int sort_check(int argc, char **argv);
+int value_check(int argc, char **argv);
 
 #endif
 
