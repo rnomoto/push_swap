c162c03d6efef681256bce6de1fdf3da073f7a5e
rnomoto <r.nomoto12345@gmail.com>
2025-05-13 11:25:25 +0900

    add atoi

--- project/main.c
+++ project/main.c
@@ -6,7 +6,7 @@
 /*   By: rnomoto <rnomoto@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 15:18:42 by rnomoto           #+#    #+#             */
-/*   Updated: 2025/05/13 08:56:15 by rnomoto          ###   ########.fr       */
+/*   Updated: 2025/05/13 11:17:14 by rnomoto          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ typedef struct s_stack
 	struct s_stack *next;
 } t_stack;
 
-t_stack *put_value_to_stack(int argc, char **argv)
+t_stack *make_stack(int argc, char **argv)
 {
 	t_stack *stack;
 	t_stack *head;
@@ -34,10 +34,9 @@ t_stack *put_value_to_stack(int argc, char **argv)
 	head = stack;
 	while (i < argc - 1)
 	{
-		stack->value = atoi(argv[i + 1]);
+		stack->value = ft_atoi(argv[i + 1]);
 		stack->index = i;
-		stack->prev = stack;
-		if (i < argc - 2)
+		if (i + 1 < argc - 1)
 		{
 			stack->next = (t_stack *)malloc(sizeof(t_stack)); //null check
 			if (stack->next == NULL)
@@ -59,13 +58,28 @@ int main(int argc, char **argv)
 {
 	t_stack *stack_a;
 
+	//if no value put
 	if (argc == 1)
 		return 0;
-	stack_a = put_value_to_stack(argc, argv);
+		
+	//check all of arguments are integer
+	if (value_check(argc, argv) == 1)
+	{
+		//ft_printf("Error. Input must be integer.\n");
+		printf("Error. Input must be integer.\n");
+		return 1;
+	}
+		
+	//if one value put or values are already sorted
+	if (argc == 2 || sort_check(argc, argv) == 0)
+		return 0;
+	
+	//make stack and put value to it.
+	stack_a = make_stack(argc, argv);
 	if (stack_a == NULL)
 		return -1;
 	
-	//list check
+	//circular linked list check
 	printf("value: %d\n", stack_a->value);
 	printf("index: %d\n\n", stack_a->index);
 	stack_a = stack_a->next;
